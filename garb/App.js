768c00e43ee4ed320c46fd812ea8f30231c4a3b8
unknown <jbwang99@ucla.edu>
2019-03-30 15:56:10 -0700

    modularization complete

--- garb/App.js
+++ garb/App.js
@@ -1,125 +1,21 @@
 import React, { Component } from 'react';
-import {Button, Text, View, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
-import { Camera, Permissions } from 'expo';
+import LoadingScreen from './includes/loading.js';
+import HomeScreen from './includes/home.js';
+import SettingsScreen from './includes/settings.js';
 
-class HomeScreen extends React.Component {
-  state = {
-    hasCameraPermission: null,
-    type: Camera.Constants.Type.back,
-    takeImageText: null,
-    photo: null,
-  };
 
-  async componentDidMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
-  }
 
-  takePicture() {
-    this.setState({
-        takeImageText: "... PROCESSING PICTURE ..."
-    });
-    this.camera.takePictureAsync({ skipProcessing: true, base64: true }).then((data) => {
-        this.setState({
-            takeImageText: "PICTURE TAKEN",
-            photo: data.base64
-        }, console.log(data.base64), this.props.navigation.navigate('Settings')
-        )
-    });
-  }
 
-  async getCameraPermissions() {
-    const { Permissions } = Expo;
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    if (status === 'granted') {
-    } else {
-      throw new Error('Camera permission not granted');
-    }
-  }
 
-  render() {
-    const { hasCameraPermission } = this.state;
-    if (hasCameraPermission === null) {
-      return <View/>;
-    } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
-    } else {
-      return (
-        <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} 
-          type={this.state.type}
-          ref={ref => { this.camera = ref; }}>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}>
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type: this.state.type === Camera.Constants.Type.back
-                      ? Camera.Constants.Type.front
-                      : Camera.Constants.Type.back,
-                  });
-                }}>
-                <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                  {' '}Flip{' '}
-                </Text>
-              </TouchableOpacity>
-            </View>
-            <View>
-              <TouchableOpacity
-              onPress={this.takePicture.bind(this)} >
-                    <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                  Take photo
-                  </Text>
-              </TouchableOpacity>
-            </View>
-          </Camera>
-        </View>
-      );
-    }
-  }
-}
-
-class SettingsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Display information from db</Text>
-      </View>
-    );
-  }
-}
-
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Details!</Text>
-      </View>
-    );
-  }
-}
 
 const HomeStack = createStackNavigator({
   Home: { screen: HomeScreen },
-  Details: { screen: DetailsScreen },
 });
 
 const SettingsStack = createStackNavigator({
   Settings: { screen: SettingsScreen },
-  Details: { screen: DetailsScreen },
 });
 
 export default createBottomTabNavigator(
